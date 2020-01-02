ca4d22e7794e8ccd5f65cb9c7529c3938bf4a258
Rafał <rafalmiskiewicz9@gmail.com>
2020-01-02 22:52:14 +0100

    naprawa

--- src/main/java/pl/rafalmiskiewicz/ADOZL/AdozlApplication.java
+++ src/main/java/pl/rafalmiskiewicz/ADOZL/AdozlApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 class AdozlApplication {
 
 	public static void main(String[] args) {
--- src/main/java/pl/rafalmiskiewicz/ADOZL/constants/AppDemoConstants.java
+++ src/main/java/pl/rafalmiskiewicz/ADOZL/constants/AppDemoConstants.java
@@ -1,5 +1,6 @@
 package pl.rafalmiskiewicz.ADOZL.constants;
 
+//ToDO to samo co w AdozlConstants
 public class AppDemoConstants {
 	
 	public static final String emailPattern = "^[a-zA-z0-9]+[\\._a-zA-Z0-9]*@[a-zA-Z0-9]+{2,}\\.[a-zA-Z]{2,}[\\.a-zA-Z0-9]*$";
