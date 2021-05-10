4ac16a5fce90f59ee90ad807f4adaaa2d2dbce2c
Rafał Miśkiewicz <rafalmiskiewicz9@gmail.com>
2021-05-10 23:54:05 +0200

    rest api jwt

--- src/main/java/pl/rafalmiskiewicz/ADOZL/AdozlApplication.java
+++ src/main/java/pl/rafalmiskiewicz/ADOZL/AdozlApplication.java
@@ -2,11 +2,6 @@ package pl.rafalmiskiewicz.ADOZL;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import pl.rafalmiskiewicz.ADOZL.rest.JetFilter;
-
-import java.util.Collections;
 
 @SpringBootApplication
 class AdozlApplication {
@@ -15,11 +10,7 @@ class AdozlApplication {
         SpringApplication.run(AdozlApplication.class, args);
     }
 
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new JetFilter());
-        filterRegistrationBean.setUrlPatterns(Collections.singleton("/api/*"));
-        return  filterRegistrationBean;
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+//    }
 }
--- src/main/java/pl/rafalmiskiewicz/ADOZL/configuration/SecurityConfig.java
+++ src/main/java/pl/rafalmiskiewicz/ADOZL/configuration/SecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -14,7 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import javax.sql.DataSource;
 
 @Configuration
-@Order(2)
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -38,16 +40,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSec) throws Exception {
         httpSec
                 .authorizeRequests()
+                .antMatchers("/hello").permitAll()
+                .antMatchers("/authenticate").permitAll()
                 .antMatchers("/").permitAll()
-                .antMatchers("/api/login").permitAll()
-                .antMatchers("/api/dupa").permitAll()
                 .antMatchers("/index").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/adduser").permitAll()
-                .antMatchers("/test1").permitAll()
-                .antMatchers("/test2").permitAll()
-//                .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                //.antMatchers("/admin").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .formLogin()
