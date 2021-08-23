512c1cbee945b3f266aba0fb5bdc0ddbc1655b0c
Rafał Miśkiewicz <rafalmiskiewicz9@gmail.com>
2021-08-23 22:27:43 +0200

    schedules

--- src/main/java/pl/rafalmiskiewicz/ADOZL/hours/HourApi.java
+++ src/main/java/pl/rafalmiskiewicz/ADOZL/hours/HourApi.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.rafalmiskiewicz.ADOZL.places.PlacesService;
+import pl.rafalmiskiewicz.ADOZL.schedule.Schedule;
+import pl.rafalmiskiewicz.ADOZL.schedule.ScheduleService;
 import pl.rafalmiskiewicz.ADOZL.user.UserService;
 import pl.rafalmiskiewicz.ADOZL.utilities.UserUtilities;
 
@@ -18,9 +21,28 @@ public class HourApi {
     @Autowired
     private UserService userService;
 
-    @RequestMapping({ "/api/hour" })
-    public String firstPage() {
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @Autowired
+    private PlacesService placesService;
+
+    @RequestMapping({"/api/hours"})
+    public String hours() {
         List<Hour> hourList = hourService.findAllByUserId(userService.findUserByEmail(UserUtilities.getLoggedUser()).getId());
-        return new Gson().toJson(hourList );
+        return new Gson().toJson(hourList);
+    }
+
+    @RequestMapping({"/api/schedules"})
+    public String schedules() {
+        List<Schedule> scheduleList = scheduleService.findAllByUserId(userService.findUserByEmail(UserUtilities.getLoggedUser()).getId());
+        for (Schedule s : scheduleList) {
+            matchPlaces(s);
+        }
+        return new Gson().toJson(scheduleList);
+    }
+
+    void matchPlaces(Schedule schedule) {
+        schedule.setPlaces(placesService.findPlacesById(schedule.getId_places()));
     }
 }
